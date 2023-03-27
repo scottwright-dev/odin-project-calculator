a444c2c345d5832753b15ace0299905b7debc8a3
Scott Wright <scott.wright.uk@gmail.com>
2023-03-27 14:20:04 +0100

    refactor: calculator functions to use operations object
    This commit refactors the calculator functions to use an operations object to map operators to their corresponding functions. The add, subtract, multiply, and divide functions have been preserved, but now the operate function uses the operations object to look up the appropriate function to call, based on the operator passed in as a parameter.
    This change makes the calculator code more flexible and easier to maintain, as new operators and functions can be added to the operations object without modifying the operate function itself.

--- script.js
+++ script.js
@@ -13,14 +13,13 @@ const operator = '';
 
 // --- OPERATE FUNCTION ----
 
+const operations = {
+    '+': add,
+    '-': subtract,
+    '*': multiply,
+    '/': divide
+};
+
 const operate = function(num1, num2, operator) {
-    if (operator === '+') {
-       return add(num1, num2);
-    } else if (operator === '-') {
-        return subtract(num1, num2);
-    } else if (operator === '*') {
-        return multiply(num1, num2);
-      }  else if (operator === '/') {
-        return divide(num1, num2);
-    }
+    return operations[operator](num1, num2);
 };
