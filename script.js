31ed6a0511e27194356ef5155b11dcf5575246ad
Scott Wright <scott.wright.uk@gmail.com>
2023-03-27 12:41:31 +0100

    Add operate function for basic arithmetic operations
    This commit adds a new function called operate, which takes two numbers and an operator and performs the requested arithmetic operation on them. The function uses the add, subtract, multiply, and divide functions that were defined earlier. The operate function improves code readability and reduces repetition.

--- script.js
+++ script.js
@@ -1,9 +1,26 @@
 // --- BASIC CALCULATION FUNCTIONS ---
 
 const add = (num1, num2) => num1 + num2;
-
 const subtract = (num1, num2) => num1 - num2;
-
 const multiply = (num1, num2) => num1 * num2;
-
 const divide = (num1, num2) => num1 / num2;
+
+// --- INITIAL CALCULATOR VARIABLES ---
+
+const num1 = 0;
+const num2 = 0;
+const operator = '';
+
+// --- OPERATE FUNCTION ----
+
+const operate = function(num1, num2, operator) {
+    if (operator === '+') {
+       return add(num1, num2);
+    } else if (operator === '-') {
+        return subtract(num1, num2);
+    } else if (operator === '*') {
+        return multiply(num1, num2);
+      }  else if (operator === '/') {
+        return divide(num1, num2);
+    }
+};
